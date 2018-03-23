3b7c289883bca2274518f9e09a2db1bb4798311f
Muneer <muneer.pp@outlook.com>
2018-03-23 11:05:25 +0530

    updated about me

---
+++ src/containers/about/about_list.js
@@ -0,0 +1,24 @@
+const ab_list = [
+  {
+    title: "Mobile APPs",
+    sub: "I can develop high performing and intuitive mobile apps for iOS and Android",
+    icon: "mobile"
+  },
+  {
+    title: "Web APPs",
+    sub: "Get web application for your projects backed by React JS and Redux",
+    icon: "web"
+  },
+  {
+    title: "REST API Development",
+    sub: "Looking for a back-end developer ? you have come to right place",
+    icon: "api"
+  },
+  {
+    title: "Training",
+    sub: "Get trained on working with react , react native , redux and more ..!",
+    icon: "dynamic"
+  }
+];
+
+export default ab_list;
--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -3,38 +3,17 @@ import Title from "../../components/Title";
 import Wrapper from "../../components/wrapper";
 import HorizontalRow from "../../components/HorizontalRow";
 import Skills from "./Skills";
-
 import Tile from "./Tile";
+import about_lst from './about_list'
 
-const ab_list = [
-  {
-    title: "Mobile APPs",
-    sub: "Fast load times and lag free interaction, my highest priority.",
-    icon: "mobile"
-  },
-  {
-    title: "Web APPs",
-    sub: "My layouts will work on any device, big or small.",
-    icon: "web"
-  },
-  {
-    title: 'REST API Development',
-    sub: "Strong preference for easy to use, intuitive UX/UI.",
-    icon: "api"
-  },
-  {
-    title: "Dynamic",
-    sub: "Websites don't have to be static, I love making pages come to life.",
-    icon: "dynamic"
-  }
-];
+
 
 export default () => {
   return (
     <Wrapper background_color="white"  j_c="flex-start">
       <Title color="black">ABOUT</Title>
       <Skills />
-      <HorizontalRow>{ab_list.map(itm => <Tile {...itm} />)}</HorizontalRow>
+      <HorizontalRow>{about_lst.map(itm => <Tile {...itm} />)}</HorizontalRow>
     </Wrapper>
   );
 };
